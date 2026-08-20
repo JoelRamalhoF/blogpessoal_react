8aaa85e472dc7ae311713104343ea60f9c22acba
Joel Ramalho Filho <joelramalho.negociosonline@gmail.com>
2026-08-20 14:48:11 -0300

    ✨ feat: adiciona gerenciamento de temas com listagem, criação, atualização e exclusão

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import Home from "./pages/home/Home";
 import Cadastro from "./pages/cadastro/Cadastro";
 import Login from "./pages/login/Login";
 import { AuthProvider } from "./contexts/AuthContext";
+import ListaTemas from "./components/tema/listatemas/ListaTemas";
+import FormTema from "./components/tema/formtema/FormTema";
+import DeletarTema from "./components/tema/deletartema/DeletarTema";
 
 
 
@@ -13,20 +16,24 @@ function App() {
 
   return (
     <>
-    <AuthProvider>
+      <AuthProvider>
 
-    
-      <BrowserRouter>
-        <Navbar />
-        <div className="min-h-[80vh]">
-          <Routes>
-            <Route path='/home' element={<Home />} />
-            <Route path='/cadastro' element={<Cadastro />} />
-            <Route path='/' element={<Login />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path='/home' element={<Home />} />
+              <Route path='/cadastro' element={<Cadastro />} />
+              <Route path='/' element={<Login />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastrartema" element={<FormTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
+              <Route path="/deletartema/:id" element={<DeletarTema />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
       </AuthProvider>
     </>
   );
